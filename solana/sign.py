1076e35dfa8905a77a2ce3a1d16af7200c73d34c
sangkara <115182304+agathasangkara@users.noreply.github.com>
2025-02-10 18:24:55 +0700

    x

--- solana/sign.py
+++ solana/sign.py
@@ -1,1 +1,9 @@
+import base58
+import base64
+from solders.keypair import Keypair
 
+def generate_signature(message: str, private_key: str) -> str:
+    private_key_bytes = base58.b58decode(private_key)
+    keypair = Keypair.from_bytes(private_key_bytes)
+    signature = keypair.sign_message(message.encode())
+    return base64.b64encode(bytes(signature)).decode()
