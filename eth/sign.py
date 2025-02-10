41614ed94c23c4670cb2d553e9fdcfb24837f34b
sangkara <115182304+agathasangkara@users.noreply.github.com>
2025-02-10 18:22:16 +0700

    x

--- eth/sign.py
+++ eth/sign.py
@@ -1,1 +1,6 @@
+from eth_keys import keys
+from eth_utils import keccak
 
+def generate_signature(message: str, private_key_hex: str):
+    private_key = keys.PrivateKey(bytes.fromhex(private_key_hex[2:]))
+    return private_key.sign_msg_hash(keccak(f"\x19Ethereum Signed Message:\n{len(message)}".encode() + message.encode())).to_hex()
